cd4c5de7533e9e237155b01fdcc03d395ea22772
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2025-04-05 00:18:21 +0300

    feat: Added constructor for AnalogJoystick

--- src/AnalogJoystick.h
+++ src/AnalogJoystick.h
@@ -7,7 +7,9 @@ namespace XC::Hardware
     class AnalogJoystick: public PositioningDeviceInterface
     {
         private:
+            int pinX;
+            int pinY;
         public:
-            AnalogJoystick(int pinX, int pinY);
+            AnalogJoystick(int pinX, int pinY): pinX(pinX), pinY(pinY) {};
     };
 }
